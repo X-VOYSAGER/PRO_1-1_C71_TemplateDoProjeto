649e2e2de1eed22239a4fd8b7f660fa31008522b
X-VOYSAGER <pjsandrade08@gmail.com>
2023-06-01 18:49:34 -0300

    BICICRETA V4

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
   //Cole sua Configuração do Firebase aqui
-  
+  const firebaseConfig = {
+  apiKey: "AIzaSyD0WHGxg519XaNs1UVH7iQfpyE80IGrErw",
+  authDomain: "bicicreta-784b0.firebaseapp.com",
+  projectId: "bicicreta-784b0",
+  storageBucket: "bicicreta-784b0.appspot.com",
+  messagingSenderId: "593216531127",
+  appId: "1:593216531127:web:4f2e6fceace2cd3ff47b32"
 };
 
 firebase.initializeApp(firebaseConfig);
